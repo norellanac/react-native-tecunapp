464d2ab51cc12a652b3d2c11ed1ce2469a4f6815
Nery Orellana <norellanac@10x.org>
2020-03-10 16:44:42 -0600

    implementacion de pantallas de tutorial, login con laravel passport. tambien se solicionan problemas con header en android, y la navegacion funciona en header y footer

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import * as React from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { SplashScreen, SafeAreaView } from "expo";
+import { Platform, StatusBar, StyleSheet, View, Text } from "react-native";
+import { AppLoading, LinearGradient } from "expo";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import useLinking from "./navigation/useLinking";
 
 import { Container, Root } from "native-base";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppIntroSlider from "react-native-app-intro-slider";
 /*/*******redux persist storage*****/
 //*****crear archivo "./src/reducers" from "App.js" */
 import { createStore, applyMiddleware } from "redux";
@@ -21,6 +16,9 @@ import { AsyncStorage } from "react-native";
 import { persistStore, persistReducer } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
+import image1 from "./assets/images/robot-dev.png";
+import image2 from "./assets/images/robot-dev.png";
+import image3 from "./assets/images/robot-dev.png";
 //const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 //redux persisting
@@ -51,61 +49,6 @@ export const apiUrl = {
 //redux persisting
 /*/*******redux persist storage*****/
 
-const Stack = createStackNavigator();
-import MainStackNavigator from "./navigation/MainStackNavigator";
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
-          Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <Provider store={store}>
-        <Container style={styles.container}>
-          <Root>
-            {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-            <PersistGate persistor={persistor} loading={null}>
-              <Stack.Screen name="Root" component={BottomTabNavigator} />
-              <MainStackNavigator />
-            </PersistGate>
-          </Root>
-        </Container>
-      </Provider>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -114,5 +57,108 @@ const styles = StyleSheet.create({
         marginTop: StatusBar.currentHeight
       }
     })
+  },
+  containerSlides: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 16
   }
 });
+
+const slides = [
+  {
+    key: "1",
+    title: "Hello All",
+    text: "Say something cool",
+    image: image1,
+    backgroundColor: "#59b2ab"
+  },
+  {
+    key: "2",
+    title: " Be Cool",
+    text: "Other cool stuff",
+    image: image2,
+    backgroundColor: "#febe29"
+  },
+  {
+    key: "3",
+    title: "Let us Start",
+    text: "We are already cool!",
+    image: image3,
+    backgroundColor: "#22bcb5"
+  }
+];
+
+import MainStackNavigator from "./navigation/MainStackNavigator";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+      showSlides: true
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
+    });
+    this.setState({ isReady: true });
+    if (store.getState().usuariosReducer.token) {
+      this.setState({ showSlides: false });
+    }
+  }
+
+  renderSlide = ({ item }) => {
+    return (
+      <View style={styles.slide}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Image source={item.image} />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    );
+  };
+  onDone = () => {
+    // User finished the introduction. Show real app through
+    // navigation or simply by controlling state
+    this.setState({ showSlides: false });
+  };
+
+  // Load any resources or data that we need prior to rendering the app
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    } else {
+      if (!this.state.showSlides) {
+        return (
+          <Provider store={store}>
+            <Container style={styles.container}>
+              <Root>
+                <PersistGate persistor={persistor} loading={null}>
+                  <MainStackNavigator />
+                </PersistGate>
+              </Root>
+            </Container>
+          </Provider>
+        );
+      } else {
+        return (
+          <AppIntroSlider
+            showSkipButton
+            renderItem={this.renderItem}
+            slides={slides}
+            onDone={this.onDone}
+          />
+        );
+      }
+    }
+  }
+}
+
+export default App;
